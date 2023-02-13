122dcfb6d0288435e146368068d063e0b4092837
quitiandaniela222 <quitiandaniela22@gmail.com>
2023-02-13 18:56:50 -0500

    new super top app

--- src/slug/Aloha.js
+++ src/slug/Aloha.js
@@ -19,8 +19,8 @@ const Aloha = () => (
             </strong>
           </h1>
           <h3>
-            Aplikasi kasir toko shop store dan restoran cafe rumah makan dengan
-            laravel php artisan.
+            Nueva aplicacion super top
+      
           </h3>
           <p>
             Mudahkan segala urursan dengan laravel apps sebuah technology modern
