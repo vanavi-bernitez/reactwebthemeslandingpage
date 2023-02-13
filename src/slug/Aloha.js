9c9cc02a776b2163edba42031dd6e55d5d8417e9
Viviana Benitez <viviana.bernitez@gmail.com>
2023-02-13 18:53:19 -0500

    Aloha file modified

--- src/slug/Aloha.js
+++ src/slug/Aloha.js
@@ -14,7 +14,7 @@ const Aloha = () => (
           <h1 className="display-2">
             <strong>
               <a href="/" className="text-danger">
-                Laravel Apps
+                ANDA LA OSA NENE
               </a>
             </strong>
           </h1>
@@ -23,10 +23,7 @@ const Aloha = () => (
             laravel php artisan.
           </h3>
           <p>
-            Mudahkan segala urursan dengan laravel apps sebuah technology modern
-            website applikasi untuk kebutuhan pembukuan dan mempercepat
-            transaksi include website untuk media promo online bagi bisnis dan
-            usaha mu.
+            Chalaaaaa es chalaaaaa no importa lo que suceda 
           </p>
           <p>Pelajari dan download e-catalaog dibawah ini</p>
           <a
